80a766b9fcb4798cd08f8a000ada70b28f2eb378
norpulatovv <yoldoshevxasan98@gmail.com>
2025-07-14 19:42:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let second = document.getElementById("secondh4");
+
+let ism = prompt("Enter text color");
+
+let bg = prompt("Enter bg color")
+
+second.style.color = ism
+
+document.body.style.backgroundColor=bg
